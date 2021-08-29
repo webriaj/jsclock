05835a105774b8fb54391ebccf0b9dcf9f75c15a
Md Riaj Khan <71939620+webriaj@users.noreply.github.com>
2021-08-29 23:45:37 +0600

    Initial commit

---
+++ js/app.js
@@ -0,0 +1,26 @@
+//select the item with DOM selction
+
+
+
+var hourTwo = document.getElementById('houR');
+var minuteTwo = document.getElementById('minute');
+var secondTwo = document.getElementById ('second');
+
+function newTime(){
+    var myTarikh = new Date();
+    var myHr = myTarikh.getHours();
+    var myMin = myTarikh.getMinutes();
+    var mySec = myTarikh.getSeconds();
+    
+    myHr = myHr % 12;
+    myHr = myHr ? myHr:12;
+    myMin = myMin < 10 ? '0'+myMin:myMin;
+    mySec = mySec<10?'0'+mySec:mySec;
+
+    hourTwo.textContent = myHr;
+    minuteTwo.textContent = myMin;
+    secondTwo.textContent = mySec;
+
+}
+
+setInterval(newTime,1000);
